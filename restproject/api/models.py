4f1c3a7951c8a9f2d7cc62128e8134e3e953b388
SuperAlina2020 <alinagithub@gmail.com>
2020-11-18 18:29:08 +0600

    created class Purchase

--- restproject/api/models.py
+++ restproject/api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Purchase(models.Model):
+    name = models.CharField(max_length=60)
+    price = models.IntegerField(default=0)
+    date_created = models.DateTimeField(auto_now_add=True)
+    quantity = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
